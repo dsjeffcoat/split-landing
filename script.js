644cd1fc29b5e888318f4c3bc5d8c95953ad4fb0
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2020-12-19 02:19:33 -0500

    Initial commit and updates with basic setup. Need to work on hover effect.

---
+++ script.js
@@ -0,0 +1,9 @@
+const left = document.querySelecter('.left')
+const right = document.querySelecter('.right')
+const container = document.querySelecter('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
